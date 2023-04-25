ffb530e674646c12d19273012b26f6fa8f0515fb
GafiAlex <130478095+GafiAlex@users.noreply.github.com>
2023-04-25 19:46:10 +0500

    Update KVNTRMPythonBot.py

--- KVNTRMPythonBot.py
+++ KVNTRMPythonBot.py
@@ -19,11 +19,15 @@ async def cats(message: types.Message):
 
         await message.reply_photo(photo, caption='Привет.')
 
-@dp.message_handler(regexp='(^Лев)')
+@dp.message_handler(regexp='(^Саша)')
 async def cats(message: types.Message):
-    with open('1573511281_0_299_2048_1451_1920x0_80_0_0_339e3316d7ec8095ca3a49d76f9e063c.jpg', 'rb') as photo:
+    with open('1216996752291.jpg.crdownload', 'rb') as photo:
 
         await message.reply_photo(photo, caption='Думаю ты понял ).')
+
+@dp.message_handler(regexp='(^Можно мне ещё поиграть в компьютер)')
+async def Da(message: types.Message):
+    await message.reply("Да.")
 
 @dp.message_handler()
 async def echo(message: types.Message):
@@ -33,19 +37,19 @@ async def echo(message: types.Message):
 
     await asyncio.sleep(2)
 
-    await types.ChatActions.upload_photo()
+    await types.ChatActions.upload_photo('')
 
     media = types.MediaGroup()
 
     media.attach_photo(types.InputFile('гг.jpg'), 'Картинка!')
 
-    media.attach_photo(types.InputFile('pic2.jpg'), 'Больше картинок!')
+    media.attach_photo(types.InputFile('AuVu5BW_bQE.jpg'), 'Больше картинок!')
 
-    media.attach_photo(types.InputFile('pic3.jpg'), 'Мем.')
+    media.attach_photo(types.InputFile('LJWhSeZnLo4.jpg'), 'Мем.')
 
     await message.reply_media_group(media=media)
 
 
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
